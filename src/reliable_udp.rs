91f4a759580815e184f760e5b41d165b989efd0e
DoctorEenot <dreenot@gmail.com>
2022-08-08 01:54:21 +0300

    implemented parsing

--- src/reliable_udp.rs
+++ src/reliable_udp.rs
@@ -11,7 +11,8 @@ pub struct Packet{
     pub seq:u32,
     pub ack:u32,
 
-    pub ptype:PType
+    pub ptype:PType,
+    pub checksum:u16
 }
 
 
@@ -21,19 +22,21 @@ impl Packet {
             return Err(packet_parsing_errors::TooSmallPacket.into());
         }
 
-        let seq:u32 = u32::from_ne_bytes(data[0..4].try_into().unwrap());
+        let seq:u32 = u32::from_be_bytes(data[0..4].try_into()?);
 
-        let ack:u32 = u32::from_ne_bytes(data[4..8].try_into().unwrap());
+        let ack:u32 = u32::from_be_bytes(data[4..8].try_into()?);
 
-        let ptype:u8 = data[8];
-        match ptype{
-            1 => {Ok(Packet{seq:seq,ack:ack,ptype:PType::Syn})},
-            2 => {Ok(Packet{seq:seq,ack:ack,ptype:PType::SynAck})},
-            3 => {Ok(Packet{seq:seq,ack:ack,ptype:PType::Ack})},
-            4 => {Ok(Packet{seq:seq,ack:ack,ptype:PType::Fin})},
-            _ => {Err(packet_parsing_errors::UknownPType::new(ptype).into())}
-        }
+        let ptype:PType = match data[8]{
+            1 => {PType::Syn},
+            2 => {PType::SynAck},
+            3 => {PType::Ack},
+            4 => {PType::Fin},
+            _ => {return Err(packet_parsing_errors::UknownPType::new(data[8]).into())}
+        };
 
+        let checksum:u16 = u16::from_be_bytes(data[8..12].try_into()?); 
         
+        Ok(Packet{seq:seq,ack:ack,ptype:ptype,checksum:checksum})
+
     }
 }
