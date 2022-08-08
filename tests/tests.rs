066c431620f6447b631ce418c6cc8ec33c99c7e2
DoctorEenot <dreenot@gmail.com>
2022-08-08 18:58:29 +0300

    added tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,1 +1,13 @@
 extern crate reliable_udp;
+use reliable_udp::packet::PType;
+
+#[test]
+fn verify_checksum(){
+    let packet = reliable_udp::packet::Packet::new(0,0,PType::Syn,5,None).unwrap();
+
+    assert_eq!(packet.checksum,261u16);
+
+    let packet = reliable_udp::packet::Packet::new(20,20,PType::Syn,5,None).unwrap();
+
+    assert_eq!(packet.checksum,301u16);
+}
